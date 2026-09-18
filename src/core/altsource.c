6d717e7672c0f338c6658f3dd5a52f84d2e71d45
XZ <199068665+XiaoZhang-qd@users.noreply.github.com>
2026-09-18 23:23:17 +0800

    Initial implementation: add src/core/altsource.c

---
+++ src/core/altsource.c
@@ -0,0 +1,4 @@
+#include "altsource.h"
+#include <string.h>
+void asc_source_init(ASCSource *source){if(source)memset(source,0,sizeof(*source));}
+void asc_app_init(ASCApp *app){if(app)memset(app,0,sizeof(*app));}
